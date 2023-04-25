e6430e89580a1e811077e2cf6c0cd73890f8927b
huynnnnn <100118053+nghinn1012@users.noreply.github.com>
2023-04-26 01:44:34 +0700

    update login user

---
+++ GreenTea/component/connection.php
@@ -0,0 +1,21 @@
+<?php
+    $db_name = 'mysql:host=localhost;db-name = shop_db';
+    $db_user = 'root';
+    $db_password = '';
+
+    $conn = new PDO($db_name, $db_user, $db_password);
+
+    if ($conn) {
+        echo "connected";
+    }  
+   
+    function unique_id() {
+        $chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charLength = strlen($chars);
+        $randomString = '';
+        for ($i=0; $i <20; $i++){
+            $randomString = $chars[mt_rand(0, $charLength-1)];
+        }
+        return $randomString;
+    }
+?>
