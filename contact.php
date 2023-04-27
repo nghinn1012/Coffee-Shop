74bfad75e5f2b32b514494015156aceb9ce67cc1
huynnnnn <100114998+huynnnnn@users.noreply.github.com>
2023-04-27 16:51:56 +0700

    commit

--- contact.php
+++ contact.php
@@ -45,7 +45,7 @@ if(isset($_POST['send'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
+   <title>Liên hệ</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -61,8 +61,8 @@ if(isset($_POST['send'])){
 <!-- header section ends -->
 
 <div class="heading">
-   <h3>contact us</h3>
-   <p><a href="home.php">home</a> <span> / contact</span></p>
+   <h3>Liên hệ với chúng tôi</h3>
+   <p><a href="home.php">Trang chủ</a> <span> / Liên hệ</span></p>
 </div>
 
 <!-- contact section starts  -->
@@ -76,12 +76,12 @@ if(isset($_POST['send'])){
       </div> -->
 
       <form action="" method="post">
-         <h3>tell us something!</h3>
-         <input type="text" name="name" maxlength="50" class="box" placeholder="enter your name" required>
-         <input type="number" name="number" min="0" max="9999999999" class="box" placeholder="enter your number" required maxlength="10">
-         <input type="email" name="email" maxlength="50" class="box" placeholder="enter your email" required>
-         <textarea name="msg" class="box" required placeholder="enter your message" maxlength="500" cols="30" rows="10"></textarea>
-         <input type="submit" value="send message" name="send" class="btn">
+         <h3>Nói tôi nghe điều bạn muốn</h3>
+         <input type="text" name="name" maxlength="50" class="box" placeholder="Tên của bạn" required>
+         <input type="number" name="number" min="0" max="9999999999" class="box" placeholder="Số điện thoại" required maxlength="10">
+         <input type="email" name="email" maxlength="50" class="box" placeholder="Email" required>
+         <textarea name="msg" class="box" required placeholder="Để lại câu hỏi tại đây..." maxlength="500" cols="30" rows="10"></textarea>
+         <input type="submit" value="Gửi" name="send" class="btn">
       </form>
 
    </div>
