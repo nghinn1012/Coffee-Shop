588ecb4906c09978b3d2400e46760fcf913e136c
huynnnnn <100118053+nghinn1012@users.noreply.github.com>
2023-04-28 10:39:25 +0700

    submit 100 + còn lọc

--- orders.php
+++ orders.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['user_id'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Chọn món</title>
+   <title>Đơn hàng</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
